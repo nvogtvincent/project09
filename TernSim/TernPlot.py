4cc17e83d930b3ff628a490b4e947e64a6dd2587
Noam <noam.vogt-vincent@st-annes.ox.ac.uk>
2021-10-11 17:22:28 +0100

    TernPlot now actually works somewhat

--- TernSim/TernPlot.py
+++ TernSim/TernPlot.py
@@ -8,10 +8,12 @@ year for a particular cell
 
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import colors
 import cmocean.cm as cm
 import cartopy.crs as ccrs
 import os
 from netCDF4 import Dataset
+from datetime import timedelta
 
 ##############################################################################
 # DIRECTORIES & PARAMETERS                                                   #
@@ -22,24 +24,24 @@ from netCDF4 import Dataset
 # PARAMETERS
 param = {# Set these flags to True/False depending on whether inputs are used
          'hist_avail'    : True,
-         'scen_avail'    : False,
+         'scen_avail'    : True,
 
          # Set these variables to the model/scenario names
          'model'         : 'UKESM1-0-LL',
          'scenario'      : 'SSP585',       # Ignored if scen_avail == False
          'hist_fn'       : 'UKESM1-0-LL_HISTORICAL_TRAJ.nc',
-         'scen_fn'       : 'UKESM1-0-LL_SSP585_TRAJ.nc',
+         'scen_fn'       : 'UKESM1-0-LL_SSP245_TRAJ.nc',
 
          # Plotting variables
-         'res'           : 2, # (degrees)
+         'res'           : 5, # (degrees)
          'bounds'        : {'lon_min' : -90,
                             'lon_max' : +65,
                             'lat_min' : -75,
                             'lat_max' : +70},
          'fig_fn'        : 'sample_output.png',
 
-         'source_bnds'   : {'lon_min' : -50,
-                            'lon_max' : -10,
+         'source_bnds'   : {'lon_min' : -60,
+                            'lon_max' : -20,
                             'lat_min' : -70,
                             'lat_max' : -70},
          'sink_lat'      : 60.,
@@ -104,14 +106,26 @@ print('Creating histogram...')
 print('')
 
 # 1. Set the starting year
-param['s_year'] = '1850' if param['hist_avail'] else '2015'
-param['s_year'] = np.datetime64(param['s_year'] + '-01-01')
+param['s_year'] = {}
+param['s_year']['hist'] = np.datetime64('1850-01-01')
+param['s_year']['scen'] = np.datetime64('2015-01-01')
 
 # 2. Convert time to years
-# Note: third line is stupid but necessary because of the awful numpy datetime
+# data['s_time'] = data['time'][:, 0]*np.timedelta64(1, 's')
+
+
 data['s_time'] = data['time'][:, 0]*np.timedelta64(1, 's')
-data['s_time'] = data['s_time'] + param['s_year']
-data['s_time'] = data['s_time'].astype('datetime64[Y]').astype(int) + 1970
+
+if param['hist_avail']:
+    data['s_time'][:hist_shape[0]] = (data['s_time'][:hist_shape[0]]+param['s_year']['hist'])
+
+    if param['scen_avail']:
+        data['s_time'][hist_shape[0]:] = (data['s_time'][hist_shape[0]:]+param['s_year']['scen'])
+else:
+    data['s_time'][:] = (data['s_time'][:]+param['s_year']['scen'])
+
+data['s_time'] = data['s_time'].astype('datetime64')
+data['s_time'] = data['s_time'].astype('datetime64[Y]').astype('S').astype(int)
 
 # Generate the binning grid
 param['Llon'] = param['bounds']['lon_max'] - param['bounds']['lon_min']
@@ -191,10 +205,12 @@ for deci in range(param['n_dec']):
             # Write a NaN if no trajectories for that latitude
             mean_traj[deci, lati, :] = np.nan
         else:
-            mean_traj[deci, lati, 0] = np.quantile(lat_lon, 0.25)
-            mean_traj[deci, lati, 1] = np.quantile(lat_lon, 0.55)
-            mean_traj[deci, lati, 2] = np.quantile(lat_lon, 0.75)
+            mean_traj[deci, lati, 0] = np.quantile(lat_lon, 0.00)
+            mean_traj[deci, lati, 1] = np.quantile(lat_lon, 0.50)
+            mean_traj[deci, lati, 2] = np.quantile(lat_lon, 1.0)
 
+mean_traj[:, 0, :] = np.nan
+mean_traj[:, -2:, :] = np.nan
 
 ##############################################################################
 # PLOTTING                                                                   #
@@ -203,7 +219,8 @@ for deci in range(param['n_dec']):
 # Plot the histogram
 
 data_crs = ccrs.PlateCarree()
-proj_crs = ccrs.Robinson(central_longitude=-20)
+# proj_crs = ccrs.Robinson(central_longitude=-20)
+proj_crs = ccrs.PlateCarree()
 
 f1 = plt.figure(figsize=(18,9))
 ax1 = plt.subplot(111, projection=proj_crs)
@@ -215,6 +232,7 @@ ax1.stock_img()
 
 plotHist = ax1.pcolormesh(lon, lat, hist.T, cmap=cm.matter,
                           transform=data_crs)
+
 start_line = ax1.plot(np.array([param['source_bnds']['lon_min'],
                                 param['source_bnds']['lon_max']]),
                       np.array([param['source_bnds']['lat_min'],
@@ -254,8 +272,8 @@ ax1.margins(x=-0.01, y=-0.01)
 # Save figure
 plt.savefig(fh['fig'], dpi=300)
 
-# Plot the line graph
 
+# Plot the line graph
 f2 = plt.figure(figsize=(18,9))
 ax2 = plt.subplot(111, projection=proj_crs)
 f2.subplots_adjust(hspace=0, wspace=0, top=0.925, left=0.1)
@@ -264,7 +282,65 @@ ax2.set_global()
 ax2.coastlines()
 ax2.stock_img()
 
+ax2.set_extent([-80, 80, -90, 90], crs=proj_crs)
+
+# Generate the colormap (from https://stackoverflow.com/questions/14777066/matplotlib-discrete-colorbar)
+dec_cmap = cm.matter
+dec_cmap_list = [dec_cmap(i) for i in range(dec_cmap.N)]
+dec_cmap = colors.LinearSegmentedColormap.from_list(
+    'decade', dec_cmap_list, dec_cmap.N)
+cmap_bnds = param['dec_list']
+norm = colors.BoundaryNorm(cmap_bnds, dec_cmap.N)
+sm = plt.cm.ScalarMappable(cmap=dec_cmap, norm=norm)
+
 for deci in range(param['n_dec']):
-    ax2.plot(mean_traj[deci, :, 1],
-             (lat[1:] + lat[:-1])/2,
-             transform=data_crs)
+    cmap_inc = int(256/param['n_dec'])
+    lim0 = ax2.plot(mean_traj[deci, :, 2],
+                    (lat[1:] + lat[:-1])/2,
+                    transform=data_crs, color=cm.matter(deci*cmap_inc),
+                    linewidth=5, zorder=deci)
+    lim1 = ax2.plot(mean_traj[deci, :, 0],
+                    (lat[1:] + lat[:-1])/2,
+                    transform=data_crs, color=cm.matter(deci*cmap_inc),
+                    linewidth=5, zorder=deci)
+
+
+start_line = ax2.plot(np.array([param['source_bnds']['lon_min'],
+                                param['source_bnds']['lon_max']]),
+                      np.array([param['source_bnds']['lat_min'],
+                                param['source_bnds']['lat_max']]),
+                      'k-', linewidth=2, transform=data_crs,
+                      zorder=100)
+
+start_bnds = ax2.scatter(np.array([param['source_bnds']['lon_min'],
+                                   param['source_bnds']['lon_max']]),
+                         np.array([param['source_bnds']['lat_min'],
+                                   param['source_bnds']['lat_max']]),
+                         c='k', marker='.', s = 100,
+                         zorder=100,
+                         transform=data_crs)
+
+finish_line = ax2.plot(np.array([-180., 180.]),
+                       np.array([param['sink_lat'], param['sink_lat']]),
+                       'k-', linewidth=2, transform=data_crs,
+                       zorder=100)
+
+target_pnt = ax2.scatter(param['target_coords']['lon'],
+                         param['target_coords']['lat'],
+                         c='r', marker='*', s=100,
+                         transform=data_crs,
+                         zorder=100)
+
+# Set up the colorbar
+axpos = ax2.get_position()
+pos_x = axpos.x0+axpos.width + 0.02
+pos_y = axpos.y0
+cax_width = 0.02
+cax_height = axpos.height
+
+pos_cax = f2.add_axes([pos_x, pos_y, cax_width, cax_height])
+
+cb = plt.colorbar(sm, cax=pos_cax)
+cb.set_label('Decade', size=12)
+ax1.set_aspect('auto', adjustable=None)
+ax1.margins(x=-0.01, y=-0.01)
