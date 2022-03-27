1fa666f28a72e60691756c37a8f21ba415ec4131
Noam <noam.vogt-vincent@st-annes.ox.ac.uk>
2022-03-27 17:26:42 +0100

    Improved plotting

--- TernSim/flight_path.py
+++ TernSim/flight_path.py
@@ -8,7 +8,7 @@ Compares flight routes across CMIP6 ensemble
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import colors
-import cmocean.cm as cm
+from matplotlib.gridspec import GridSpec
 import cartopy.crs as ccrs
 import os
 from netCDF4 import Dataset
@@ -29,7 +29,7 @@ param = {'terns_per_release': 2000,
          'fig_fn': 'tern_routes.png',
 
          # Plotting variables
-         'res'           : 5, # (degrees)
+         'res'           : 2, # (degrees)
          'bounds'        : {'lon_min' : -90,
                             'lon_max' : +60,
                             'lat_min' : -75,
@@ -44,7 +44,7 @@ param = {'terns_per_release': 2000,
          'sink_lat'      : 60.,
          'target_coords' : {'lon'     : -20,
                             'lat'     : 65},
-         'cmap1'          : cmr.torch_r,
+         'cmap1'          : cmr.flamingo_r,
          'cmap2'          : cmr.fusion_r
          }
 
@@ -97,10 +97,13 @@ y_bnd = np.arange(param['bounds']['lat_min'],
 
 grid = np.zeros((3, len(y_bnd)-1, len(x_bnd)-1), dtype=np.float64) # HIST/245/585
 
+n_traj_tot = [0, 0, 0]
+
 # Grid the model output
 for model_i, model in enumerate(model_names):
     for scenario_i, scenario in enumerate(['HISTORICAL', 'SSP245', 'SSP585']):
-        output_name = dirs['traj'] + model_names[i] + '_' + scenario + '_TRAJ.nc'
+        output_name = dirs['traj'] + model + '_' + scenario + '_TRAJ.nc'
+        print('Gridding ' + scenario + ' ' + model + '...')
 
         with Dataset(output_name, mode='r') as nc:
             t0 = nc.variables['time'][:, 0]
@@ -126,15 +129,40 @@ for model_i, model in enumerate(model_names):
                 traj_idx = np.where(np.isin(year_list, scen_years))[0]
 
             # Load and grid data
-            lon = nc.variables['lon'][traj_idx, :].compressed()
-            lat = nc.variables['lat'][traj_idx, :].compressed()
+            lon = nc.variables['lon'][traj_idx, :]
+            lat = nc.variables['lat'][traj_idx, :]
+            n_traj = np.shape(lon)[0]+1 # Number of unique terns gathered
+            n_traj_tot[scenario_i] += n_traj
 
-            grid[scenario_i, :, :] += np.histogram2d(lon, lat,
-                                                     bins=[x_bnd, y_bnd],
-                                                     normed=True)[0].T
+            tid = np.zeros_like(lon) # Trajectory identifier
+            tid[:] = np.arange(np.shape(lon)[0]).reshape(-1, 1)+1
+            tid = np.ma.masked_array(tid, mask=lon.mask).compressed()
+            lon = lon.compressed()
+            lat = lat.compressed()
 
-# Remove grids with extremely small (insiginificant) tern numbers
-grid[grid < 1e-4] = 0
+            # Split arrays if necessary
+            n_splits = n_traj*len(x_bnd)*len(y_bnd)/5e8
+            n_splits = 1 if n_splits < 1 else int(np.ceil(n_splits))
+
+            lon = np.array_split(lon, n_splits)
+            lat = np.array_split(lat, n_splits)
+            tid = np.array_split(tid, n_splits)
+
+            # Now calculate the number of unique trajectories passing through each cell, and normalise
+            for i in range(len(lon)):
+                grid_ = np.histogramdd(np.array([lon[i], lat[i], tid[i]]).T, bins=[x_bnd, y_bnd, np.arange(tid[i].min(), tid[i].max()+2, 1)-0.5])[0]
+                grid_[grid_ > 1] = 1
+                grid[scenario_i, :, :] += np.sum(grid_, axis=2).T
+
+for i in range(3):
+    grid[i, :, :] /= n_traj_tot[i]
+
+# Now calculate differences
+# grid[0] = HIST, grid[1] = SSP245, grid[2] = SSP585, grid[3] = SSP245-HIST, grid[4] = SSP585-HIST
+grid = np.concatenate([grid,
+                       (grid[1,:,:]-grid[0,:,:]).reshape(1, np.shape(grid)[1], np.shape(grid)[2]),
+                       (grid[2,:,:]-grid[0,:,:]).reshape(1, np.shape(grid)[1], np.shape(grid)[2])],
+                       axis=0)
 
 ##############################################################################
 # PLOT DATA                                                                  #
@@ -144,203 +172,88 @@ grid[grid < 1e-4] = 0
 # HISTORICAL TERN LOCATIONS ##################################################
 ##############################################################################
 
-# Plotting ocean trajectories
-f, ax = plt.subplots(1, 1, figsize=(15, 10),
-                     subplot_kw={'projection': ccrs.Robinson(central_longitude=-30)})
+f = plt.figure(constrained_layout=True, figsize=(27, 20))
+gs = GridSpec(2, 6, figure=f, width_ratios=[2, 1, 0.05, 0.1, 1, 0.05])
+ax = []
+ax.append(f.add_subplot(gs[:, 0], projection = ccrs.Robinson(central_longitude=-30)))
+ax.append(f.add_subplot(gs[0, 1], projection = ccrs.Robinson(central_longitude=-30)))
+ax.append(f.add_subplot(gs[1, 1], projection = ccrs.Robinson(central_longitude=-30)))
+ax.append(f.add_subplot(gs[0, 4], projection = ccrs.Robinson(central_longitude=-30)))
+ax.append(f.add_subplot(gs[1, 4], projection = ccrs.Robinson(central_longitude=-30)))
+ax.append(f.add_subplot(gs[:, 2]))
+ax.append(f.add_subplot(gs[:, 5]))
+label_list = ['a', 'b', 'd', 'c', 'e']
+
 data_crs = ccrs.PlateCarree()
 
-f.subplots_adjust(hspace=0, wspace=0, top=0.925, left=0.1)
-ax.set_aspect(1)
-ax.set_extent([-95, 35, -90, 90], crs=data_crs)
+f.subplots_adjust(hspace=0.08, wspace=0.08)
 
-land_10m = cfeature.NaturalEarthFeature('physical', 'land', '10m',
-                                        edgecolor='face',
-                                        facecolor='black')
-
-# Add start/finish line
-start_line = ax.plot(np.array([param['source_bnds']['lon_min'],
-                                param['source_bnds']['lon_max']]),
-                      np.array([param['source_bnds']['lat_min'],
-                                param['source_bnds']['lat_max']]),
-                      'k-', linewidth=2, transform=data_crs,
-                      zorder=100)
-
-start_bnds = ax.scatter(np.array([param['source_bnds']['lon_min'],
-                                   param['source_bnds']['lon_max']]),
-                         np.array([param['source_bnds']['lat_min'],
-                                   param['source_bnds']['lat_max']]),
-                         c='k', marker='.', s = 100,
-                         zorder=100,
-                         transform=data_crs)
-
-target_pnt = ax.scatter(param['target_coords']['lon'],
-                         param['target_coords']['lat'],
-                         c='k', marker='+', s=100,
-                         transform=data_crs,
-                         zorder=100)
-
-
-# Set up the colorbar
-pos_cax = f.add_axes([ax.get_position().x1+0.015,ax.get_position().y0-0.0,0.015,ax.get_position().height])
-
-# Add cartographic features
-gl = ax.gridlines(crs=data_crs, draw_labels=True,
-                  linewidth=0.5, color='black', linestyle='--', zorder=11)
-gl.xlocator = mticker.FixedLocator(np.arange(-210, 210, 60))
-gl.ylocator = mticker.FixedLocator(np.arange(-90, 120, 30))
-gl.xlabels_top = False
-gl.ylabels_right = False
+gl = []
+hist = []
 
 land_10m = cfeature.NaturalEarthFeature('physical', 'land', '10m',
                                         edgecolor='face',
                                         facecolor='gray',
                                         zorder=1)
-ax.add_feature(land_10m)
 
-# Plot the colormesh
-cmap1 = param['cmap1']
+for i in range(5):
+    ax[i].set_aspect(1)
+    ax[i].set_extent([-85, 25, -90, 90], crs=data_crs)
 
-hist = ax.pcolormesh(x_bnd, y_bnd, np.ma.masked_where(grid[0]==0, grid[0]),
-                     cmap=cmap1, vmin=0, vmax=np.max(grid[0]),
-                     transform=ccrs.PlateCarree(), zorder=2,
-                     alpha=0.8)
+    # Add start/finish line
+    start_line = ax[i].plot(np.array([param['source_bnds']['lon_min'],
+                                      param['source_bnds']['lon_max']]),
+                            np.array([param['source_bnds']['lat_min'],
+                                      param['source_bnds']['lat_max']]),
+                            'k-', linewidth=2, transform=data_crs,
+                            zorder=100)
 
-cb = plt.colorbar(hist, cax=pos_cax)
-cb.set_label('Simulated Arctic Tern locations on northbound migration, 1950-1999', size=12)
+    start_bnds = ax[i].scatter(np.array([param['source_bnds']['lon_min'],
+                                         param['source_bnds']['lon_max']]),
+                               np.array([param['source_bnds']['lat_min'],
+                                         param['source_bnds']['lat_max']]),
+                               c='k', marker='.', s = 100,
+                               zorder=100,
+                               transform=data_crs)
 
-##############################################################################
-# PAST/FUTURE DIFFERENCES (SSP245) ###########################################
-##############################################################################
+    target_pnt = ax[i].scatter(param['target_coords']['lon'],
+                               param['target_coords']['lat'],
+                               c='k', marker='+', s=100,
+                               transform=data_crs,
+                               zorder=100)
 
-# Plotting ocean trajectories
-f, ax = plt.subplots(1, 1, figsize=(15, 10),
-                     subplot_kw={'projection': ccrs.Robinson(central_longitude=-30)})
-data_crs = ccrs.PlateCarree()
+    # Add cartographic features
+    gl.append(ax[i].gridlines(crs=data_crs, draw_labels=True,
+                              linewidth=0.5, color='black', linestyle='--', zorder=11))
+    gl[i].xlocator = mticker.FixedLocator(np.arange(-210, 210, 60))
+    gl[i].ylocator = mticker.FixedLocator(np.arange(-90, 120, 30))
+    gl[i].xlabels_top = False
+    gl[i].ylabels_right = False
+    gl[i].ylabels_left = False if i > 0 else True
+    gl[i].ylabel_style = {'size': 18}
+    gl[i].xlabel_style = {'size': 18}
 
-f.subplots_adjust(hspace=0, wspace=0, top=0.925, left=0.1)
-ax.set_aspect(1)
-ax.set_extent([-95, 35, -90, 90], crs=data_crs)
+    ax[i].add_feature(land_10m)
 
-land_10m = cfeature.NaturalEarthFeature('physical', 'land', '10m',
-                                        edgecolor='face',
-                                        facecolor='black')
+    # Plot the colormesh
+    if i <= 2:
+        cmap = param['cmap1']
+        hist.append(ax[i].contourf(0.5*(x_bnd[:-1]+x_bnd[1:]), 0.5*(y_bnd[:-1]+y_bnd[1:]), 100*grid[i, :, :],
+                                   cmap=cmap, levels=np.linspace(2, 20, num=10), transform=data_crs,
+                                   zorder=2, extend='max'))
+    else:
+        cmap = param['cmap2']
+        hist.append(ax[i].contourf(0.5*(x_bnd[:-1]+x_bnd[1:]), 0.5*(y_bnd[:-1]+y_bnd[1:]), 100*np.ma.masked_where(grid[i, :, :] == 0, grid[i, :, :]),
+                             cmap=cmap, levels=np.linspace(-4, 4, num=17), transform=data_crs,
+                             zorder=2, extend='both'))
 
-# Add start/finish line
-start_line = ax.plot(np.array([param['source_bnds']['lon_min'],
-                                param['source_bnds']['lon_max']]),
-                      np.array([param['source_bnds']['lat_min'],
-                                param['source_bnds']['lat_max']]),
-                      'k-', linewidth=2, transform=data_crs,
-                      zorder=100)
+    xpos = -122 if i == 0 else -120
 
-start_bnds = ax.scatter(np.array([param['source_bnds']['lon_min'],
-                                   param['source_bnds']['lon_max']]),
-                         np.array([param['source_bnds']['lat_min'],
-                                   param['source_bnds']['lat_max']]),
-                         c='k', marker='.', s = 100,
-                         zorder=100,
-                         transform=data_crs)
+    ax[i].text(xpos, -85, label_list[i], transform=data_crs, fontsize=30, va='bottom', ha='left', fontweight='bold')
 
-target_pnt = ax.scatter(param['target_coords']['lon'],
-                         param['target_coords']['lat'],
-                         c='k', marker='+', s=100,
-                         transform=data_crs,
-                         zorder=100)
+cb1 = plt.colorbar(hist[0], cax=ax[5])
+cb1.set_label('Percentage of vTerns passing through cell on northbound migration', size=18)
 
+cb2 = plt.colorbar(hist[3], cax=ax[6])
+cb2.set_label('Change in vTern percentage passing through cell for Scenario vs Historical', size=18)
 
-# Set up the colorbar
-pos_cax = f.add_axes([ax.get_position().x1+0.015,ax.get_position().y0-0.0,0.015,ax.get_position().height])
-
-# Add cartographic features
-gl = ax.gridlines(crs=data_crs, draw_labels=True,
-                  linewidth=0.5, color='black', linestyle='--', zorder=11)
-gl.xlocator = mticker.FixedLocator(np.arange(-210, 210, 60))
-gl.ylocator = mticker.FixedLocator(np.arange(-90, 120, 30))
-gl.xlabels_top = False
-gl.ylabels_right = False
-
-land_10m = cfeature.NaturalEarthFeature('physical', 'land', '10m',
-                                        edgecolor='face',
-                                        facecolor='gray',
-                                        zorder=1)
-ax.add_feature(land_10m)
-
-# Plot the colormesh
-cmap2 = param['cmap2']
-grid10 = grid[1]/grid[0]
-
-hist = ax.pcolormesh(x_bnd, y_bnd, np.ma.masked_where(grid10==0, grid10),
-                     cmap=cmap2, norm=colors.LogNorm(vmin=0.1, vmax=10),
-                     transform=ccrs.PlateCarree(), zorder=2, alpha=0.8)
-
-cb = plt.colorbar(hist, cax=pos_cax)
-cb.set_label('Normalised tern trajectory difference for SSP245 vs Historical, [2050-2099]/[1950-1999]', size=12)
-
-##############################################################################
-# PAST/FUTURE DIFFERENCES (SSP585) ###########################################
-##############################################################################
-
-# Plotting ocean trajectories
-f, ax = plt.subplots(1, 1, figsize=(15, 10),
-                     subplot_kw={'projection': ccrs.Robinson(central_longitude=-30)})
-data_crs = ccrs.PlateCarree()
-
-f.subplots_adjust(hspace=0, wspace=0, top=0.925, left=0.1)
-ax.set_aspect(1)
-ax.set_extent([-95, 35, -90, 90], crs=data_crs)
-
-land_10m = cfeature.NaturalEarthFeature('physical', 'land', '10m',
-                                        edgecolor='face',
-                                        facecolor='black')
-
-# Add start/finish line
-start_line = ax.plot(np.array([param['source_bnds']['lon_min'],
-                                param['source_bnds']['lon_max']]),
-                      np.array([param['source_bnds']['lat_min'],
-                                param['source_bnds']['lat_max']]),
-                      'k-', linewidth=2, transform=data_crs,
-                      zorder=100)
-
-start_bnds = ax.scatter(np.array([param['source_bnds']['lon_min'],
-                                   param['source_bnds']['lon_max']]),
-                         np.array([param['source_bnds']['lat_min'],
-                                   param['source_bnds']['lat_max']]),
-                         c='k', marker='.', s = 100,
-                         zorder=100,
-                         transform=data_crs)
-
-target_pnt = ax.scatter(param['target_coords']['lon'],
-                         param['target_coords']['lat'],
-                         c='k', marker='+', s=100,
-                         transform=data_crs,
-                         zorder=100)
-
-
-# Set up the colorbar
-pos_cax = f.add_axes([ax.get_position().x1+0.015,ax.get_position().y0-0.0,0.015,ax.get_position().height])
-
-# Add cartographic features
-gl = ax.gridlines(crs=data_crs, draw_labels=True,
-                  linewidth=0.5, color='black', linestyle='--', zorder=11)
-gl.xlocator = mticker.FixedLocator(np.arange(-210, 210, 60))
-gl.ylocator = mticker.FixedLocator(np.arange(-90, 120, 30))
-gl.xlabels_top = False
-gl.ylabels_right = False
-
-land_10m = cfeature.NaturalEarthFeature('physical', 'land', '10m',
-                                        edgecolor='face',
-                                        facecolor='gray',
-                                        zorder=1)
-ax.add_feature(land_10m)
-
-# Plot the colormesh
-cmap2 = param['cmap2']
-grid20 = grid[2]/grid[0]
-
-hist = ax.pcolormesh(x_bnd, y_bnd, np.ma.masked_where(grid20==0, grid20),
-                     cmap=cmap2, norm=colors.LogNorm(vmin=0.1, vmax=10),
-                     transform=ccrs.PlateCarree(), zorder=2, alpha=0.8)
-
-cb = plt.colorbar(hist, cax=pos_cax)
-cb.set_label('Normalised tern trajectory difference for SSP585 vs Historical, [2050-2099]/[1950-1999]', size=12)
-plt.savefig(fh['fig'], dpi=300)
